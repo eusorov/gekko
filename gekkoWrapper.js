541e934ac6bea4db7bed0b1d2f74d71173d00eaa
eusorov <eusorov@googlemail.com>
2018-11-17 13:18:02 +0100

    js wrapper

--- gekkoWrapper.js
+++ gekkoWrapper.js
@@ -1,2 +1,8 @@
-#!/bin/sh
-exec /opt/plesk/node/9/bin/ "$@" --config sample-config.js --ui
+//#!/bin/sh
+//exec /opt/plesk/node/9/bin/node "$@" --config sample-config.js --ui
+
+process.argv[1]='--config';
+process.argv[2]='sample-config.js';
+process.argv[3]='--ui';
+
+const gekko = require('./gekko.js');
