c559fb8753991e71eb669a426e5c54e4dcdead44
eusorov <eusorov@googlemail.com>
2018-11-24 15:16:24 +0100

    list on env.PORT

--- web/server.js
+++ web/server.js
@@ -122,8 +122,8 @@ app
   .use(router.allowedMethods());
 server.timeout = config.api.timeout || 120000;
 server.on('request', app.callback());
-server.listen({port : config.api.port} , () => {
-  const host = `${config.ui.host}:${config.ui.port}${config.ui.path}`;
+server.listen({port : process.env.PORT} , () => {
+  const host = `${config.ui.host}:${process.env.PORT}${config.ui.path}`;
 
   if(config.ui.ssl) {
     var location = `https://${host}`;
